b40a76199af324cd426d559b707f1c0ce7a26ade
dhanush <dhanushpk50@gmail.com>
2024-03-17 10:50:14 +0530

    changes

--- src/component/Analysis.js
+++ src/component/Analysis.js
@@ -4,7 +4,7 @@ import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 
-const Analysis = ( {setUploadedData} ) => {
+const Analysis = ({ setUploadedData }) => {
     const [loading, setLoading] = useState(false);
     const [result, setResult] = useState(false);
     const [selectionRange, setSelectionRange] = useState({
@@ -48,7 +48,7 @@ const Analysis = ( {setUploadedData} ) => {
         }
         setLoading(true);
         setUploadedData(true);
-    
+
         // Simulate loading for 10 seconds
         setTimeout(() => {
             // Handle form submission here
@@ -57,7 +57,7 @@ const Analysis = ( {setUploadedData} ) => {
             setResult(true);
         }, 10000); // 10 seconds in milliseconds
     };
-    
+
 
     const handleSelect = (ranges) => {
         setSelectionRange({
@@ -68,7 +68,7 @@ const Analysis = ( {setUploadedData} ) => {
 
     return (
         <form ref={form} onSubmit={handleSubmit}>
-            {!loading && !result && (
+            {!result && (
                 <>
                     <div className="space-y-12 lg:m-8 m-4">
                         <div className="border-b border-gray-900/10 pb-12">
@@ -115,19 +115,39 @@ const Analysis = ( {setUploadedData} ) => {
                     </div>
                 </>
             )}
-            {
-                loading && (
-                    <div className="mt-6 flex items-center m-8 gap-x-6">
-                        <p className="text-sm font-semibold leading-6 text-gray-900">Loading...</p>
+            {loading && (
+                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+                    <div aria-label="Loading..." role="status" className="flex flex-col items-center space-y-4">
+                        <svg className="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
+                            <line x1="128" y1="32" x2="128" y2="64" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                            <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                            <line x1="224" y1="128" x2="192" y2="128" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                            <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                            <line x1="128" y1="224" x2="128" y2="192" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                            <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                            <line x1="32" y1="128" x2="64" y2="128" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                            <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                        </svg>
+                        <span className="text-4xl font-medium text-gray-500">Loading...</span>
                     </div>
-                )
-            }
+                </div>
+            )}
+
             {
                 result && (
-                    <div className="mt-6 flex items-center m-8 gap-x-6">
-                        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                            Download
-                        </button>
+                    // < !--Main modal -->
+                    <div className=" overflow-y-auto overflow-x-hidden z-50 justify-center items-center w-full  h-modal md:h-full">
+                        <div className="relative p-4 w-full h-full md:h-auto">
+                            {/* <!-- Modal content --> */}
+                            <div className="relative p-4 text-center bg-white rounded-lg w-full sm:p-5">
+                                <div className="w-12 h-12 rounded-full bg-green-100 dark:bg-green-900 p-2 flex items-center justify-center mx-auto mb-3.5">
+                                    <svg aria-hidden="true" className="w-8 h-8 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                                    <span className="sr-only">Success</span>
+                                </div>
+                                <p className="mb-4 text-lg font-semibold text-gray-900 ">Successfully uploaded the data.</p>
+                                <p className="mb-4 text-sm text-gray-500 ">Your data has been successfully uploaded and is being processed.</p>
+                            </div>
+                        </div>
                     </div>
                 )
             }
